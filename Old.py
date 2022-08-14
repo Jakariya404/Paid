65584d3ff478c3dd0c72203e48e7dc47cccda96f
Jakariya404 <111103375+Jakariya404@users.noreply.github.com>
2022-08-14 18:13:04 +0600

    Update Old.py

--- Old.py
+++ Old.py
@@ -2618,7 +2618,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrkausar-cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get("https://github.com/maminhaz60/MINHAZ/blob/main/Approved.txt").text
+	r1=requests.get("https://github.com/Jakariya404/Paid/blob/main/Approved.txt").text
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
